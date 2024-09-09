dab811d3333b45ae08ce0ec12692b944e8abc640
Michael Mendy <montana@linux.com>
2024-09-09 12:08:55 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+const http = require('http');
+
+// Create an HTTP server and listen on port 3000
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
