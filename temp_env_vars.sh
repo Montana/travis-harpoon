e1fbc6fc07aba619bc6cf5b53f3abe99bae53edd
Michael Mendy <montana@linux.com>
2024-09-09 15:40:17 -0700

    push script via vim

---
+++ temp_env_vars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export HARPOON_API_TOKEN="$1"
+export DOCKER_USERNAME="$2"
+export DOCKER_PASSWORD="$3"
+
+echo "HARPOON_API_TOKEN added: $HARPOON_API_TOKEN"
+echo "DOCKER_USERNAME added: $DOCKER_USERNAME"
+echo "DOCKER_PASSWORD added: [hidden]"
